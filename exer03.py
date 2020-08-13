d597a27da6c485cb2048b8c61d2e04eab9d3d05d
mechu1205 <mechu1205@gmail.com>
2020-08-13 21:45:30 +0900

    Update exer03.py
    how to optim

--- exer03.py
+++ exer03.py
@@ -22,51 +22,86 @@ model = torch.nn.Sequential(
 # case we will use Mean Squared Error (MSE) as our loss function.
 loss_fn = torch.nn.MSELoss(reduction='sum')
 
-learning_rate = 1e-4
+
+# without optim
+
+# learning_rate = 1e-4
+# for t in range(500):
+#     # Forward pass: compute predicted y by passing x to the model. Module objects
+#     # override the __call__ operator so you can call them like functions. When
+#     # doing so you pass a Tensor of input data to the Module and it produces
+#     # a Tensor of output data.
+#     y_pred = model(x)
+    
+#     # Compute and print loss. We pass Tensors containing the predicted and true
+#     # values of y, and the loss function returns a Tensor containing the
+#     # loss.
+#     loss = loss_fn(y_pred, y)
+#     if t % 100 == 99:
+#         print(t, loss.item())
+        
+#     # Zero the gradients before running the backward pass.
+#     model.zero_grad()
+    
+#     '''
+#     In PyTorch, we need to set the gradients to zero before starting to do backpropragation
+#     because PyTorch accumulates the gradients on subsequent backward passes.
+#     This is convenient while training RNNs.
+#     So, the default action is to accumulate (i.e. sum) the gradients on every loss.backward() call.
+#     Because of this, when you start your training loop,
+#     ideally you should zero out the gradients so that you do the parameter update correctly.
+#     Else the gradient would point in some other direction
+#     than the intended direction towards the minimum (or maximum, in case of maximization objectives).
+#     '''
+    
+#     # Backward pass: compute gradient of the loss with respect to all the learnable
+#     # parameters of the model. Internally, the parameters of each Module are stored
+#     # in Tensors with requires_grad=True, so this call will compute gradients for
+#     # all learnable parameters in the model.
+#     loss.backward()
+    
+#     # Update the weights using gradient descent. Each parameter is a Tensor, so
+#     # we can access its gradients like we did before.
+#     with torch.no_grad():
+#         # temporarily sets all require_grad to False
+#         '''
+#         To prevent tracking history (and using memory),
+#         you can also wrap the code block in with torch.no_grad()
+#         This can be particularly helpful when evaluating a model
+#         because the model may have trainable parameters with requires_grad=True,
+#         but for which we don’t need the gradients.
+#         '''
+#         for param in model.parameters():
+#             param -= learning_rate * param.grad
+
+
+# with optim
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 for t in range(500):
-    # Forward pass: compute predicted y by passing x to the model. Module objects
-    # override the __call__ operator so you can call them like functions. When
-    # doing so you pass a Tensor of input data to the Module and it produces
-    # a Tensor of output data.
+    # Forward pass: compute predicted y by passing x to the model.
     y_pred = model(x)
     
-    # Compute and print loss. We pass Tensors containing the predicted and true
-    # values of y, and the loss function returns a Tensor containing the
-    # loss.
+    # Compute and print loss.
     loss = loss_fn(y_pred, y)
     if t % 100 == 99:
         print(t, loss.item())
-        
-    # Zero the gradients before running the backward pass.
-    model.zero_grad()
     
-    '''
-    In PyTorch, we need to set the gradients to zero before starting to do backpropragation
-    because PyTorch accumulates the gradients on subsequent backward passes.
-    This is convenient while training RNNs.
-    So, the default action is to accumulate (i.e. sum) the gradients on every loss.backward() call.
-    Because of this, when you start your training loop,
-    ideally you should zero out the gradients so that you do the parameter update correctly.
-    Else the gradient would point in some other direction
-    than the intended direction towards the minimum (or maximum, in case of maximization objectives).
-    '''
+    # Before the backward pass, use the optimizer object to zero all of the
+    # gradients for the variables it will update (which are the learnable
+    # weights of the model). This is because by default, gradients are
+    # accumulated in buffers( i.e, not overwritten) whenever .backward()
+    # is called. Checkout docs of torch.autograd.backward for more details.
+    optimizer.zero_grad()
     
-    # Backward pass: compute gradient of the loss with respect to all the learnable
-    # parameters of the model. Internally, the parameters of each Module are stored
-    # in Tensors with requires_grad=True, so this call will compute gradients for
-    # all learnable parameters in the model.
+    # Backward pass: compute gradient of the loss with respect to model
+    # parameters
     loss.backward()
     
-    # Update the weights using gradient descent. Each parameter is a Tensor, so
-    # we can access its gradients like we did before.
-    with torch.no_grad():
-        # temporarily sets all require_grad to False
-        '''
-        To prevent tracking history (and using memory),
-        you can also wrap the code block in with torch.no_grad()
-        This can be particularly helpful when evaluating a model
-        because the model may have trainable parameters with requires_grad=True,
-        but for which we don’t need the gradients.
-        '''
-        for param in model.parameters():
-            param -= learning_rate * param.grad
+    # Calling the step function on an Optimizer makes an update to its
+    # parameters
+    optimizer.step()
+    
+    
+    # where is the gradients calculated at loss.backward() stored?
+    # how can optimizer.step() access them?
